6b888c703881c4c07967e8f773eee901a2efa0ad
lucianarocharod <luciana.rocha@scanntech.com>
2026-06-22 10:36:16 -0300

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,9 +1,7 @@
 # locals.tf — Valores calculados internamente por Terraform
- 
 locals {
   # Prefijo único que combina las variables — se usa en todos los nombres
   resource_prefix = "clase9-${var.prefix}-${var.location}"
- 
   # Tags que se aplican a todos los recursos del proyecto
   common_tags = {
     Environment = "clase9"
